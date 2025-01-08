6c8bbae35eb87121af015e7c8d195a73fa3648f8
Jimenez <FNJimenez@cps.k12.il.us>
2025-01-08 12:46:32 -0600

    needwork

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -52,6 +52,12 @@
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
 
 # Don't call the function, you just need to define it.
+# number=[1,2,3] # list of number 
+# def all_positive(number): # the function 
+#     for n in number: # created to see what number are inside 
+#         if n < 0:  # if they are less than 0 they negative 
+#             return False
+#     return False
 
 
 
@@ -60,11 +66,29 @@
 
 # Dynamic Functions Practice #2
 # Create a function (sum_less) that adds the numbers of a list as long as they are greater than 0 and less than 1000, and returns the result of said sum. Create a numbers variable, storing a list of numbers so we can test it.
-
-
+# num= [300,50,500] # list of number 
+# def sum_less(num): # create function 
+#     total=0 
+#     for n in num: # defining function 
+#         if n> 0 and n <1000:
+#             total +=n
+#     return total 
+# print(sum_less(num))
 
 
 
 
 # Dynamic Functions Practice #3
 # Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
+
+# def count_even(first):
+
+# def count_even(numbers):
+#     even_numbers=0
+#     for n in numbers:
+#         if n%2 ==0:
+#             even_numbers = even_numbers+1
+#         else:
+#             pass
+#     return even_numbers
+# variable= [124,43,5343]
